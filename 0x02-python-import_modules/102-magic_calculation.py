0ddafce9a8c4850ecc9afa8153885bfe01c9051b
Wassimou <islawassim@gmail.com>
2023-10-05 03:47:52 -0700

    updates 3

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -4,16 +4,11 @@ def magic_calculation(a, b):
     """Match bytecode provided by Holberton School."""
     from magic_calculation_102 import add, sub
 
-    print(f"a: {a}, b: {b}")  # Add this line for debugging
-
     if a < b:
         c = add(a, b)
         for i in range(4, 6):
             c = add(c, i)
-        return c
-    else:
-        return sub(a, b)
+        return (c)
 
-# Call the function for debugging purposes
-result = magic_calculation(5, 10)
-print(f"Result: {result}")
+    else:
+        return(sub(a, b))
