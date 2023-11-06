ec350712c4c1068b811847fae1273c831ae378ff
Wassimou <islawassim@gmail.com>
2023-11-06 08:56:53 -0800

    updates

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,14 +1,7 @@
 #!/usr/bin/python3
-"""
-===================================
-module with class Rectangle
-===================================
-"""
-
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 class Rectangle(BaseGeometry):
-    """Rectangle class that inherits from BaseGeometry"""
-
     def __init__(self, width, height):
         self.integer_validator("width", width)
         self.__width = width
